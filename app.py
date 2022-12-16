1b63b9628872777ea555eec1fa15e072f28b3b8d
Ahmed_Hossam <61332730+Ahmed-Hossam-Aldeen@users.noreply.github.com>
2022-12-16 16:03:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ if input_image:
     image = image.reshape(1, 256,256,3)
 
     prediction = test.predict(image)
-    if prediction > 0.07:
+    if prediction > 0.03:
        final = "بقدونس"
-    elif prediction <0.07:
+    elif prediction <0.03:
        final = "كزبرة"    
 
     
